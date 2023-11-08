f6a90eb54901f9dd7e754e0f47612586be6e9b65
B07207012 <b07207012@ntu.edu.tw>
2023-11-08 11:13:37 +0800

    feat: modify minSdkVersion and compileVersion

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+// can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+// Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+// Get a specific camera from the list of available cameras.
+  for (var c in cameras) print(c);
+
   runApp(const MyApp());
 }
 
